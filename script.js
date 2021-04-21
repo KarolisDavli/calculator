8481b69dd74d2f211e53da09a1a4b3720f87609c
Karolis Davli <kdavlijevas@gmail.com>
2021-04-21 22:43:44 +0100

    equal functionality

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let currentOperator = document.querySelector('.current');
-let previousOperator = document.querySelector('.previous');
+let currentOperatorDisplay = document.querySelector('.current');
+let previousOperatorDisplay = document.querySelector('.previous');
 let operand;
 let numbers = document.querySelectorAll('[data-number]');
+let operations = document.querySelectorAll('[data-operation]');
+let equals = document.querySelector('[data-equals]');
+let currentNumber;
+let previousNumber;
 
 
 // Math functions
@@ -41,10 +45,42 @@ function operate(num1, num2, operator) {
 // On click numbers
 numbers.forEach(number => {
   number.addEventListener('click', function() {
-    currentOperator.innerHTML += number.innerHTML;
+    currentOperatorDisplay.innerHTML += number.innerHTML;
+    currentNumber = currentOperatorDisplay.innerHTML;
+    console.log('dog');
   })
 })
 
-console.log('Inner html is:' + currentOperator.innerHTML);
-console.log(previousOperator.innerHTML);
+// On click operation
+operations.forEach(operation => {
+  operation.addEventListener('click', function() {
+    previousOperatorDisplay.innerHTML = currentOperatorDisplay.innerHTML;
+    currentOperatorDisplay.innerHTML = '';
+    previousNumber = previousOperatorDisplay.innerHTML;
+    operand = this.innerHTML;
+    console.log('pusi');
+    console.log(this.innerHTML);
+  })
+})
+
+// Equals
+equals.addEventListener('click', function() {
+  operate(currentNumber, previousNumber, operand);
+  console.log('equality');
+})
+
+
+
+console.log('Inner html is:' + currentOperatorDisplay.innerHTML);
+console.log(previousOperatorDisplay.innerHTML);
 console.log(numbers);
+console.log(currentNumber);
+console.log(previousNumber);
+console.log(operations.innerHTML);
+
+
+
+//  Problema dabar tokia, kad num1 ir num2 yra sukeisti vietoj, 
+// Tai vietoj 5 - 3 gaunas 3 - 5.
+// Tai pat rezultatas nera displayinamas
+// C mygtuko funcionalumas
